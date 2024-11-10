1ee9bf3977ec5e5891dbc2cd12380770c375c3b1
SaiHarishMK <164997628+SaiHarishMK@users.noreply.github.com>
2024-11-10 12:01:16 +0530

    Enrollment.java

---
+++ Enrollment.java
@@ -0,0 +1,11 @@
+public class Enrollment {
+    private Student student;
+    private Course course;
+    private String grade;
+
+    public Enrollment(Student student, Course course, String grade) {
+        this.student = student;
+        this.course = course;
+        this.grade = grade;
+    }
+}
